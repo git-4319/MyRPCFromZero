d94707ff8073d9be02331c5d7a951dae13659bd5
programmer 4319 <1601537248@qq.com>
2022-11-20 20:31:01 +0800

    myRPCVersion1:添加了一些注释

--- src/main/java/com/ganghuan/myRPCVersion1/client/ClientProxy.java
+++ src/main/java/com/ganghuan/myRPCVersion1/client/ClientProxy.java
@@ -16,12 +16,28 @@ public class ClientProxy implements InvocationHandler {
 
 
     // jdk 动态代理， 每一次代理对象调用方法，会经过此方法增强（反射获取request对象，socket发送至客户端）
+
+    /**
+     * subject.SayHello("jiankunking")这句话时，会自动调用InvocationHandlerImpl的invoke方法
+     * Java 动态代理，具体有如下四步骤：
+     * 1.通过实现 InvocationHandler 接口创建自己的调用处理器；
+     * 2.通过为 Proxy 类指定 ClassLoader 对象和一组 interface 来创建动态代理类；
+     * 3.通过反射机制获得动态代理类的构造函数，其唯一参数类型是调用处理器接口类型；
+     * 4.通过构造函数创建动态代理类实例，构造时调用处理器对象作为参数被传入。
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // request的构建，使用了lombok中的builder，代码简洁
-        RPCRequest request = RPCRequest.builder().interfaceName(method.getDeclaringClass().getName())
+        RPCRequest request = RPCRequest.builder()
+                .interfaceName(method.getDeclaringClass().getName())
                 .methodName(method.getName())
-                .params(args).paramsTypes(method.getParameterTypes()).build();
+                .params(args)
+                .paramsTypes(method.getParameterTypes()).build();
         //数据传输
         RPCResponse response = IOClient.sendRequest(host, port, request);
         //System.out.println(response);
--- src/main/java/com/ganghuan/myRPCVersion1/server/UserServiceImpl.java
+++ src/main/java/com/ganghuan/myRPCVersion1/server/UserServiceImpl.java
@@ -12,7 +12,8 @@ public class UserServiceImpl implements UserService {
         System.out.println("客户端查询了"+id+"的用户");
         // 模拟从数据库中取用户的行为
         Random random = new Random();
-        User user = User.builder().userName(UUID.randomUUID().toString())
+        User user = User.builder()
+                .userName(UUID.randomUUID().toString())
                 .id(id)
                 .sex(random.nextBoolean()).build();
         return user;
